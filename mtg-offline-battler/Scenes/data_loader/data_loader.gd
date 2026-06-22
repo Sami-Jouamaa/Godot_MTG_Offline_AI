aa5990c60521d6057914b8679f49867baa6844e9
Sami-Jouamaa <samijouamaa@gmail.com>
2026-06-22 13:52:12 -0400

    http request are done for bulk data file and the default_cards json, i need to organise the default_cards json in two json files containing relevant fields for card info and the printings associated with each oracle_id

--- mtg-offline-battler/Scenes/data_loader/data_loader.gd
+++ mtg-offline-battler/Scenes/data_loader/data_loader.gd
@@ -1,19 +1,63 @@
 extends Node
 
-var requestJSON
+var local_update_at
+var remote_update_at
 
-func _ready() -> void:
-	$HTTPRequest.request_completed.connect(_on_request_completed)
+var default_cards_url
+
+var is_importing = false
 
 func load_data():
-	print("hello")
-	# make http request to scryfall for cards' info and images
-	
-	# this is just the different types of bulk data files
-	# use the Default cards download link, contains every print of every english cards
-	# for example, Double major has 5 prints, the "Default cards" json file contains 5 entries of Double major, one for each print
-	# await $HTTPRequest.request("https://api.scryfall.com/bulk-data")
+	load_meta()
+	await $bulk_data_file.request("https://api.scryfall.com/bulk-data")
 
-func _on_request_completed(result, response_code, headers, body):
-	requestJSON = JSON.parse_string(body.get_string_from_utf8())
-	print(requestJSON)
+func load_meta():
+	if FileAccess.file_exists("user://meta.json"):
+		var file = FileAccess.open("user://meta.json", FileAccess.READ)
+		var data = JSON.parse_string(file.get_as_text())
+		local_update_at = data.get("default_cards_updated_at", "")
+
+func should_retry(response):
+	return response == 408 or response == 429 or response >= 500
+	
+func retry_download(url):
+	await get_tree().create_timer(5.0).timeout
+	await $bulk_data_file.request(url)
+
+func _on_bulk_data_file_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	if (should_retry(response_code)):
+		await retry_download("https://api.scryfall.com/bulk-data")
+		return
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	for bulk in json["data"]:
+		if (bulk["type"] == "default_cards"):
+			remote_update_at = bulk["updated_at"]
+			if (should_update()):
+				default_cards_url = bulk["download_uri"]
+				await $default_cards_file.request(bulk["download_uri"])
+
+func update_meta_file():
+	var meta = {
+		"default_cards_updated_at": remote_update_at
+	}
+	var file = FileAccess.open("user://meta.json", FileAccess.WRITE)
+	file.store_string(JSON.stringify((meta), "\t"))
+	file.close()
+
+func _on_default_cards_file_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	if (should_retry(response_code)):
+		await retry_download(default_cards_url)
+		return
+	# var default_cards = JSON.parse_string(body.get_string_from_utf8())
+	# to filter the data and build the json data files
+	
+	# block user input and display something to say that the cards are being imported
+	is_importing = true
+	# await save_cards(default_cards)
+	is_importing = false
+	update_meta_file()
+
+func should_update():
+	if local_update_at == null:
+		return true
+	return remote_update_at > local_update_at
