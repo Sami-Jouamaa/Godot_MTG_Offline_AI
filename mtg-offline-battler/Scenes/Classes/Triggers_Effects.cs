f9e4a66fd9edbfaad7494c2fbb4afe8707ada4a9
Sami-Jouamaa <samijouamaa@gmail.com>
2026-07-17 20:31:41 -0400

    added some triggers and enum for counters

--- mtg-offline-battler/Scenes/Classes/Triggers_Effects.cs
+++ mtg-offline-battler/Scenes/Classes/Triggers_Effects.cs
@@ -1,4 +1,32 @@
 using System;
+// the way target(s) is stored will probably be changed depending on how targeting cards and players will work
+
+public enum CounterType
+{
+    zero_one,
+    zero_two,
+    zero_three,
+    zero_four,
+    zero_five,
+    one_zero,
+    one_one,
+    one_two,
+    one_three,
+    one_four,
+    one_five,
+    two_zero,
+    two_one,
+    two_two,
+    two_three,
+    two_four,
+    two_five,
+    Flying,
+    Indestructible,
+    Lifelink,
+    Radiation,
+    Haste
+    // to continue
+}
 
 public enum Triggers
 {
@@ -10,5 +38,28 @@ public enum Triggers
 
 public class DrawCards
 {
-    int Amount {get; set;} = 0;
+    public int DrawAmount = 0;
+    public string Target = "";
 }
+
+public class DiscardCards
+{
+    public int DiscardAmount = 0;
+    public string Target = "";
+}
+
+public class GainOrLoseLife
+{
+    // can be negative
+    public int LifeAmount = 0;
+    public string Target = "";
+}
+
+public class Counters
+{
+    public CounterType TypeOfCounter = 0;
+    public int CountersAmount = 0;
+    public int NbCreatures = 0;
+    public string Target = "";
+}
+
